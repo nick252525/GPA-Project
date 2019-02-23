a740abc27becb560d07bb275a02a9269ddf135ae
nick252525 <ntandle2@illinois.edu>
2019-02-23 00:40:20 +0000

    Beginning the creation of the Shiny app

---
+++ GPA_Projection/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("GPA Projection"),
+
+   fluidRow(
+     column(2,
+            textInput(inputId = 'abb',
+                      label = "Letter Course Name and Number",
+                      value = total_course_info$abbreviation),
+            
+            textInput(inputId = "credit",
+                      label = "Enter desired credit hour if not already provided",
+                      value = total_course_info$credit)
+                        )
+     
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
