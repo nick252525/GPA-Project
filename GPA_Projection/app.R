d19851988347a52b75911fbae30f7cf707143e81
nick252525 <ntandle2@illinois.edu>
2019-03-27 01:40:18 +0000

    Tweaking user interface and eliminating error when no values have been chosen

--- GPA_Projection/app.R
+++ GPA_Projection/app.R
@@ -1,42 +1,51 @@
 library(shinythemes)
 
 ui= fluidPage(theme = shinytheme("united"),
-
+              
     # Application title
-    titlePanel("GPA Projection"),
-
+    
+    p(strong(titlePanel(
+      "G.P.A. Projection Tool"
+    ))), br(),
+    
     sidebarLayout(
-      sidebarPanel(
+      sidebarPanel( 
         selectInput(
-            inputId = "course_name",
-            label = "Type in course name and numbers",
-            multiple = TRUE,
-            choices = c(as.character(total_course_info$abbreviation))
+          inputId = "course_name",
+          label = "Type in course name and numbers",
+          multiple = TRUE,
+          choices = c(as.character(total_course_info$abbreviation))
         ),
         
-        submitButton()
-        ),
-     
-    mainPanel(tabsetPanel(type = "tab", 
-                          tabPanel("Projected Weighted GPA", textOutput("textt")),
-                          tabPanel("Summary of Course Info", tableOutput("sum_table"))))
+        submitButton("Submit")
+      ),
+      
+      mainPanel(verticalLayout(
+        h3("Summary of Course Info", style = "color:darkblue"), 
+        strong(h4(tableOutput("table"))),
+        
+        h2(align = "center"),
+        
+        h3("Projected Weighted GPA", style = "color:darkblue"),
+        strong(h4(textOutput("text")))
+      ), align = "center")
     ))
-
 
 
 server <- function(input, output) {
 
- 
-  output$sum_table = renderTable({
-    (summary_fun(input$course_name))
-  })
-  
+output$table = renderTable({
+      validate(need(!is.null(input$course_name), "Please enter course names"))
+    summary_fun(input$course_name)
+})
 
-  output$textt= renderText({Semester_gpa_fun(input$course_name)})
-  
-  
-   
+
+output$text = renderText({
+    validate(need(!is.null(input$course_name), "Please enter course names"))
+    Semester_gpa_fun(input$course_name)    
+      })
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
